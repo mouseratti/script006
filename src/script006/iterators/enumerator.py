8e49ec8035fb2a6f4166837e639dcca6a03e469b
Mikhail Koshel <mouseratti@gmail.com>
2018-10-09 23:03:08 +0600

    Added: tests for package iterator

--- src/script006/iterators/enumerator.py
+++ src/script006/iterators/enumerator.py
@@ -1,7 +1,11 @@
+"""
+Create your own enumerate implementation in this module
+https://docs.python.org/3/library/functions.html#enumerate
+"""
 from typing import (
     Iterable,
     Generator
 )
-def my_enumerate(i: Iterable) -> Generator:
+def my_enumerate(i: Iterable, start: int = 0) -> Generator:
     # type your code here
     pass
--- test/script006/iterators/enumerator_test.py
+++ test/script006/iterators/enumerator_test.py
@@ -1,3 +1,20 @@
+import pytest
+
+from script006.iterators import my_enumerate
 
 
+def test_enumerator():
+    inputted = [x for x in range(10)]
+    tested, expected = my_enumerate(inputted), enumerate(inputted)
+    with pytest.raises(StopIteration):
+        while True:
+            assert next(tested) == next(expected)
 
+
+def test_enumerator_with_nonzero_start():
+    inputted = [x for x in range(10)]
+    start = 5
+    tested, expected = my_enumerate(inputted, start), enumerate(inputted, start)
+    with pytest.raises(StopIteration):
+        while True:
+            assert next(tested) == next(expected)
