19111f9ecfec1268353cd3424d801b5deea5bfb9
Mikhail Koshel <mouseratti@gmail.com>
2018-10-10 10:04:44 +0600

    updated repo

---
+++ src/script006/iterators/itertools_example.py
@@ -0,0 +1,28 @@
+import itertools
+from string import digits, ascii_lowercase
+
+def chain():
+    i1 = (_ for _ in ('one', 'two', 'three'))
+    i2 = [letter for letter in ascii_lowercase]
+    i3 = []
+    yield from itertools.chain(i1,i2,i3)
+
+
+def count():
+    yield from itertools.count(20, 5)
+
+def zip():
+    itertools.zip()
+
+
+if __name__ == '__main__':
+    g1 = chain()
+    while True:
+        try:
+            print(next(g1))
+        except StopIteration:
+            break
+
+    g2 = count()
+    print([next(g2) for _ in range(3)])
+    print(next(g2))
