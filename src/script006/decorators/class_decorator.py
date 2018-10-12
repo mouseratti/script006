3787e6a75da5b6cc975c8dacc8b86c643aff1fa3
Mikhail Koshel <mouseratti@gmail.com>
2018-10-12 10:11:28 +0600

    added decorators

---
+++ src/script006/decorators/class_decorator.py
@@ -0,0 +1,43 @@
+class DecorClass(object):
+    def __init__(self, func):
+        self.func = func
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs) * 2
+        pass
+
+class WrapperWithParams(object):
+    def __init__(self, multiplier):
+        self.multiplier = multiplier
+        pass
+
+    def __call__(self, func, *args, **kwargs):
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs) * self.multiplier
+        return wrapper
+
+
+
+
+@DecorClass
+def decorated1(i):return i
+
+@WrapperWithParams(3)
+def decorated3(i):return i
+
+
+@WrapperWithParams(4)
+def decorated4(i):return i
+
+
+
+if __name__ == '__main__':
+    fn1 = decorated1
+    print(fn1(1))
+
+    fn3 = decorated3
+    print(fn3(1))
+
+    fn4 = decorated4
+    print(fn4(1))
