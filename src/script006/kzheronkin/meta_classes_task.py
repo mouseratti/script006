e5430d826b68c8ab4a999bd91999df4cb6185dc5
spb20 <spb20@ptc.local>
2018-10-15 13:14:03 +0300

    Class generater added

--- src/script006/kzheronkin/meta_classes_task.py
+++ src/script006/kzheronkin/meta_classes_task.py
@@ -1,32 +1,36 @@
 
 def repr(this): return "object of {}".format(this.__class__)
 
-class SimpleMetaClass(type):
 
-    def __prepare__(name, inheritance):
-        return {"свойство":"НОВОЕ"}
+class MetaClassForUpperCase(type):
 
-    def __new__(mcs,name,inheritance, props):
-        props['proprety_added_by_metaclass'] = SimpleMetaClass.__name__
-        new_class = super().__new__(mcs,name,inheritance, props)
+    def __new__(mcs, name, inheritance, props):
+        for prop in props:
+            if prop[0:2] != "__":
+                props[str.upper(prop)] = props.pop(prop)
+        new_class = super().__new__(mcs, name, inheritance, props)
         return new_class
 
-    def __init__(cls, clsname, inheritance, props):
-        super().__init__(clsname, inheritance, props)
 
-
-
-class B(object, metaclass=SimpleMetaClass):
+class B(object, metaclass= MetaClassForUpperCase):
 
     prop1 ="prop1"
+
     def __repr__(self):
         return "instance of {} with id {}".format(self.__class__, id(self))
 
+    def __init__(self):
+        self.sailor = "Long John"
+        self.doctor = "Doctor Livesley"
 
+
+def fake_init(object):
+    print("The new lord object has arrived.")
 
 
 if __name__ == '__main__':
     b = B()
-    c = SimpleMetaClass("C", (), {"__repr__": repr})()
-    print("{}: {}".format(b, b.proprety_added_by_metaclass))
-    print("{}: {}".format(c, c.proprety_added_by_metaclass))
+    c = MetaClassForUpperCase("C", (), {"__init__": fake_init, "__repr__": repr, "abc": "my_abc"})
+
+    c_object = c()
+
